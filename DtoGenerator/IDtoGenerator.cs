b04df3e82abebd01b82951bb9d56749dda552de8
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-06 20:12:54 +0300

    Create IDtoGenerator interface

---
+++ DtoGenerator/IDtoGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace DtoGenerator
+{
+    public interface IDtoGenerator
+    {
+        T Create<T>();
+        object[] GetConstructorArguments(ConstructorInfo ctor);
+    }
+}
