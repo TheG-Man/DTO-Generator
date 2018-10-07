277b3c39b04b21a4226a417eb121f5c2d6e8c77f
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-07 16:41:06 +0300

    Create AgeGenerator class

---
+++ DtoGenerator/AgeGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtoGenerator
+{
+    public class AgeGenerator: IRandomValueGenerator
+    {
+        private readonly Random _random;
+
+        public string Type
+        {
+            get { return "Byte"; }
+        }
+
+        public AgeGenerator()
+        {
+            _random = new Random();
+        }
+
+        public object Generate<T>()
+        {
+            byte b = (byte)_random.Next(1, 47);
+
+            return b;
+        }
+    }
+}
