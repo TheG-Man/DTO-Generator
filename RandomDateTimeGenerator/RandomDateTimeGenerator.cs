9b187932f6ecbbbc2edebbd874583a39304a827b
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-07 16:25:08 +0300

    Create RandomDateTimeGenerator class

---
+++ RandomDateTimeGenerator/RandomDateTimeGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DtoGenerator;
+
+namespace RandomDateTimeGenerator
+{
+    public class RandomDateTimeGenerator: IRandomValueGenerator
+    {
+        private readonly Random _random;
+
+        public string Type
+        {
+            get { return "DateTime"; }
+        }
+
+        public RandomDateTimeGenerator()
+        {
+            _random = new Random();
+        }
+
+        public object Generate<T>()
+        {
+            DateTime init = new DateTime(1945, 6, 2);
+            int range = (DateTime.Today - init).Days;
+
+            return init.AddDays(_random.Next(range));
+        }
+    }
+}
