331c06f21fd2f580c5abb0b2e51bfc066304102e
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-07 16:49:09 +0300

    Create ConfigDescription class

---
+++ DtoGenerator/ConfigDescription.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtoGenerator
+{
+    public class ConfigDescription
+    {
+        public Type ObjectType { get; }
+        public string PropertyName { get; }
+        public Type RandomValueGeneratorType { get; }
+
+        public ConfigDescription(Type objectType, string propertyName, Type randomGeneratorType)
+        {
+            ObjectType = objectType;
+            PropertyName = propertyName;
+            RandomValueGeneratorType = randomGeneratorType;
+        }
+    }
+}
