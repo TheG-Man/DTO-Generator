0f3d39006de39f283ed1ad4320948483c0e22627
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-07 16:23:34 +0300

    Create RandomByteGenerator class

---
+++ DtoGenerator/RandomByteGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtoGenerator
+{
+    public class RandomByteGenerator : IRandomValueGenerator
+    {
+        private readonly Random _random;
+
+        public string Type
+        {
+            get { return "Byte"; }
+        }
+
+        public RandomByteGenerator()
+        {
+            _random = new Random();
+        }
+
+        public object Generate<T>()
+        {
+            byte[] b = new byte[1];
+
+            _random.NextBytes(b);
+
+            return b[0];
+        }
+    }
+}
