a08353e8597b63aa2b6b8d9d76c88f477f26e118
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-07 16:23:57 +0300

    Create RandomBoolGenerator class

---
+++ DtoGenerator/RandomBoolGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtoGenerator
+{
+    public class RandomBoolGenerator: IRandomValueGenerator
+    {
+        public string Type
+        {
+            get { return "Boolean"; }
+        }
+
+        public object Generate<T>()
+        {
+            return (object)true;
+        }
+    }
+}
