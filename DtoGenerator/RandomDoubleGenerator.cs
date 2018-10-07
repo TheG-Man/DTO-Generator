dc8d3e8bbb47e4fc0bf4726eb5f6b3df6636c22e
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-07 16:23:45 +0300

    Create RandomDoubleGenerator class

---
+++ DtoGenerator/RandomDoubleGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtoGenerator
+{
+    public class RandomDoubleGenerator : IRandomValueGenerator
+    {
+        private readonly Random _random;
+
+        public string Type
+        {
+            get { return "Double"; }
+        }
+
+        public RandomDoubleGenerator()
+        {
+            _random = new Random();
+        }
+
+        public object Generate<T>()
+        {
+            return _random.NextDouble();
+        }
+    }
+}
